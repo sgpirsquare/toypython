30f6acaee3a1ce65c8980b922c07f4dae05ee7de
sgpirsquare <69654439+sgpirsquare@users.noreply.github.com>
2023-06-20 13:12:42 +0900

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,12 @@
+#1부터 시작하는 피보나치 수열 재귀함수
+
 def fibonacci(n):
-  if n <= 1:
+  if n <= 0:
     return n
   else:
     return fibonacci(n-1) + fibonacci(n - 2)
 
-a = input("2 이상 자연수를 입력하세요: ")
+a = input("Enter a natural number greater than or equal to 2: ")
 
 for i in range(int(a)):
   print(fibonacci(i))
